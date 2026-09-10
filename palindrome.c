5b459d1bf37b46fee2d3f16a4fabb3d1f61f2a5c
Sameer Sahu <manojsahu.developer@gmail.com>
2026-09-11 00:54:23 +0530

    created armstrong and palindrome number

--- palindrome.c
+++ palindrome.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
 int main(){
     int num;
-    int arr;
-}
+    printf("Enter The Number: ");
+    scanf("%d", &num);
+    int originalNum = num;
+    int reversedNum = 0;
+    while (num != 0) {
+        int digit = num % 10;
+        reversedNum = reversedNum * 10 + digit;
+        num /= 10;
+    }
+    if (originalNum == reversedNum) {
+        printf("%d is a palindrome number.\n", originalNum);
+    } else {
+        printf("%d is not a palindrome number.\n", originalNum);
+    }
+    return 0;
+}   
