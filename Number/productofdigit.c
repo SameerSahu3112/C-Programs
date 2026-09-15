0ee4b9c15b450e633ce6cebcd681d41e91abc78e
Sameer Sahu <manojsahu.developer@gmail.com>
2026-09-16 00:45:42 +0530

    2 more programs done

---
+++ Number/productofdigit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n, product = 1, digit;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n == 0) {
+        product = 0; 
+    } else {
+        while (n > 0) {
+            digit = n % 10; 
+            product *= digit; 
+            n /= 10; 
+        }
+    }
+    printf("Product of digits: %d\n", product);
+    return 0;
+}
---
+++ Number/sumofodddigit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,digit,sum=0;
+    printf("Enter The Num: ");
+    scanf("%d", &n);
+
+    while (n>0){
+        digit = n % 10;
+        if (digit % 2 != 0){
+            sum += digit;
+        }
+        n /= 10;
+    }
+    printf("Sum Of Even Digits: %d", sum);
+}
